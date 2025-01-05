2c009e9ff9f0474ba1071ac964b17c11937578f3
PrinceTheDev <sinbadprince9@gmail.com>
2025-01-05 22:23:05 +0100

    started working on the home views and templates

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Welcome to the Novacrest Hospital')
 
 # Create your views here.
